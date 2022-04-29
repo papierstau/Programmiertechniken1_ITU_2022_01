a2d280287f3e55d777bc38c869ec4c25dfe31847
Alexander Schwarz <alexander_schwarz@live.de>
2022-04-29 15:06:48 +0200

    2 Tag Online

---
+++ PHP/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        #Ich bin Zeilenkommentar
+        //Ich bin ebenfalls ein Zeilenkommentar
+        /*
+            Ich bin eine Mehrzeilenkommentar
+        */
+        $_ichBinEineZahl = 1; #Integer
+        $ichBinEineZahl = 15;
+        $IchBinEineZahl = 5; #Integer
+        $_ich_bin_ein_string = 'Wort'; //String
+        $fliessKommaZahl = 2.3; //Float
+        $ichBinEinBool = false; //Boolean
+        
+        echo ($_ich_bin_ein_string);
+        print ("<br>Hallo Welt<br>");
+        var_dump($_ich_bin_ein_string);
+        
+        ECHO($ichBinEineZahl ."<br>");
+        
+        eChO($IchBinEineZahl);
+
+    ?>
+</body>
+</html>
