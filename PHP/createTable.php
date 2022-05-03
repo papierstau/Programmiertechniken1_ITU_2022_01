2dbfeb298f012cd5a74bd52d878d87f48b4e7b26
papierstau <85675051+papierstau@users.noreply.github.com>
2022-05-03 22:38:25 +0200

    Update createTable.php

--- PHP/createTable.php
+++ PHP/createTable.php
@@ -4,7 +4,7 @@
     $person = 'users';
 
     #Ersstellen einer Tabelle mit SQL und PHP
-    $createTable1 = $conn->query('CREATE TABLE users(
+    $createTable1 = $connection->query('CREATE TABLE users(
                     id INT(255) AUTO_INCREMENT NOT NULL, #id als Primärschlüssel - Immer nur einen pro Tabelle
                     user VARCHAR(255) NOT NULL,
                     password VARCHAR(255) NOT NULL,
